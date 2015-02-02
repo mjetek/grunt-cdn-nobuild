cfb605bc1e90b176b25c8cac10aa9f3487c72ffe
Rui Araújo <ruka.araujo@gmail.com>
2015-02-02 02:28:39 +0100

    Fix regex to handle misses in the link group.

--- tasks/cdn_nobuild.js
+++ tasks/cdn_nobuild.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
     var options = this.options({
     });
 
-    var cdnTagsRe = /(<script\ssrc\s*=\s*['"](?:[a-zA-Z]+:)?\/\/[^<]+<\/script>\r?\n?)|(<link\s+(?:\w*\s*[="]*)*\s+href\s*=\s*['"](?:[a-zA-Z]+:)?\/\/[^<>]+\/?>\r?\n?)/gim;
+    var cdnTagsRe = /(<script\ssrc\s*=\s*['"](?:[a-zA-Z]+?:)?\/\/[^<]+<\/script>)|(<link\s+(?:\w*=['"].*?['"])*?\s+href\s*=\s*['"](?:[a-zA-Z]+:)?\/\/[^<>]+?\/?>)/gim;
     var buildSectionBeginRe = /<!--\s*build:/gim;
     var buildSectionEndRe = /<!--\s*endbuild/gim;
 
@@ -60,7 +60,7 @@ module.exports = function(grunt) {
           cdnScriptTags.push(cdnMatch[0]);
         }
         var newBuildSection = buildSection.replace(cdnTagsRe, '');
-        newBuildSection = cdnScriptTags.join('') + newBuildSection;
+        newBuildSection = cdnScriptTags.join('\n') + newBuildSection;
 
         sectionsData.push({
           begin: beginMatch.index,
